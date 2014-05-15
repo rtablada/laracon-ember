6504735c159864864064d7cfd7d3c2a6f7e3e6c7
Ryan Tablada <ryan.tablada@gmail.com>
2014-05-15 14:32:14 -0400

    Test some things out now

--- gallery/app/router.js
+++ gallery/app/router.js
@@ -6,6 +6,7 @@ Router.map(function() {
 	this.resource('posts', function() {
 		this.route('create', { path: 'new' });
 		this.route('show', { path: ':post_id' });
+		this.route('edit', { path: ':post_id/edit' });
 	});
 });
 
