1fff089e222f116eba3468ae5c06ad8d8c61c81c
Ryan Tablada <ryan.tablada@gmail.com>
2014-05-15 14:51:37 -0400

    Fixes the body parsing for proxy requests...

--- gallery/server/index.js
+++ gallery/server/index.js
@@ -8,15 +8,15 @@
 // };
 
 var express    = require('express');
-var bodyParser = require('body-parser');
+// var bodyParser = require('body-parser');
 var globSync   = require('glob').sync;
 var routes     = globSync('./routes/*.js', { cwd: __dirname }).map(require);
 
 module.exports = function(emberCLIMiddleware) {
   var app = express();
-  app.use(bodyParser());
+  // app.use(bodyParser());
 
-  routes.forEach(function(route) { route(app); });
+  // routes.forEach(function(route) { route(app); });
   app.use(emberCLIMiddleware);
 
   return app;
